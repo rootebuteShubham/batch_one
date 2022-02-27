a1c4b7b701ed8c11a9d20b698115db47a78732fe
rootebuteShubham <40230891+rootebuteShubham@users.noreply.github.com>
2022-02-27 13:22:02 +0530

    Create BookService.java

---
+++ BookService.java
@@ -0,0 +1,16 @@
+package com.greatlearning.lmsapp;
+
+import java.util.List;
+
+public interface BookService {
+
+	public List<Book> findAll();
+	
+	public List<Book> searchBy(String name, String author);
+	
+	public Book findById(int id);
+	
+	public void save(Book book); // save or update
+	
+	public void deleteById(int id);
+}
